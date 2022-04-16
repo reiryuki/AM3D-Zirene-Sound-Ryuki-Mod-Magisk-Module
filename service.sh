95d7f90df81f916ea8631209505f95e3ad9ad45e
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-04-16 19:20:55 +0700

    Enhancements

--- service.sh
+++ service.sh
@@ -2,37 +2,63 @@
 
 MODPATH=${0%/*}
 API=`getprop ro.build.version.sdk`
+AML=/data/adb/modules/aml
 
 # debug
 exec 2>$MODPATH/debug.log
 set -x
 
+# prevent soft reboot
+echo 0 > /proc/sys/kernel/panic
+echo 0 > /proc/sys/kernel/panic_on_oops
+echo 0 > /proc/sys/kernel/panic_on_rcu_stall
+echo 0 > /proc/sys/kernel/panic_on_warn
+echo 0 > /proc/sys/vm/panic_on_oom
+
+# function
+restart_audioserver() {
+if [ "$API" -ge 24 ]; then
+  killall audioserver
+else
+  killall mediaserver
+fi
+}
+
+# restart
+restart_audioserver
+
 # wait
 sleep 20
 
 # mount
-AML=/data/adb/modules/aml
+NAME="*audio*effects*.conf -o -name *audio*effects*.xml -o -name *policy*.conf -o -name *policy*.xml"
 if [ ! -d $AML ] || [ -f $AML/disable ]; then
   DIR=$MODPATH/system/vendor
 else
   DIR=$AML/system/vendor
 fi
-FILE=`find $DIR/odm/etc -maxdepth 1 -type f -name *audio*effects*.conf\
-      -o -name *audio*effects*.xml -o -name *audio*policy*.conf\
-      -o -name *stage*policy*.conf -o -name *audio*policy*.xml`
-if [ "$FILE" ]; then
+FILE=`find $DIR/odm/etc -maxdepth 1 -type f -name $NAME`
+if [ "`realpath /odm/etc`" != /vendor/odm/etc ] && [ "$FILE" ]; then
   for i in $FILE; do
     j="$(echo $i | sed "s|$DIR||")"
     umount $j
     mount -o bind $i $j
   done
+  restart_audioserver
 fi
-
-# restart
-if [ "$API" -ge 24 ]; then
-  killall audioserver
+if [ ! -d $AML ] || [ -f $AML/disable ]; then
+  DIR=$MODPATH/system
 else
-  killall mediaserver
+  DIR=$AML/system
+fi
+FILE=`find $DIR/etc -maxdepth 1 -type f -name $NAME`
+if [ -d /my_product/etc ] && [ "$FILE" ]; then
+  for i in $FILE; do
+    j="$(echo $i | sed "s|$DIR||")"
+    umount /my_product$j
+    mount -o bind $i /my_product$j
+  done
+  restart_audioserver
 fi
 
 # wait
