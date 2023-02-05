f1631f688754a295ef27b86eb96db242503cb4f6
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2023-02-05 09:21:40 +0700

    Enhancements

--- service.sh
+++ service.sh
@@ -6,6 +6,17 @@ AML=/data/adb/modules/aml
 exec 2>$MODPATH/debug.log
 set -x
 
+# restart
+if [ "$API" -ge 24 ]; then
+  SVC=audioserver
+else
+  SVC=mediaserver
+fi
+PID=`pidof $SVC`
+if [ "$PID" ]; then
+  killall $SVC
+fi
+
 # wait
 sleep 20
 
@@ -62,26 +73,65 @@ if [ ! -d $MY_PRODUCT ] && [ -d /my_product/etc ]\
   done
 fi
 
-# restart
-if [ "$API" -ge 24 ]; then
-  PID=`pidof audioserver`
-  if [ "$PID" ]; then
-    killall audioserver
-  fi
-else
-  PID=`pidof mediaserver`
-  if [ "$PID" ]; then
-    killall mediaserver
-  fi
-fi
-
 # wait
-sleep 40
+until [ "`getprop sys.boot_completed`" == "1" ]; do
+  sleep 10
+done
 
 # allow
 PKG=com.fihtdc.am3dfx
 if [ "$API" -ge 30 ]; then
   appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
 fi
+
+# function
+stop_log() {
+FILE=$MODPATH/debug.log
+SIZE=`du $FILE | sed "s|$FILE||"`
+if [ "$LOG" != stopped ] && [ "$SIZE" -gt 50 ]; then
+  exec 2>/dev/null
+  LOG=stopped
+fi
+}
+check_audioserver() {
+if [ "$NEXTPID" ]; then
+  PID=$NEXTPID
+else
+  PID=`pidof $SVC`
+fi
+sleep 10
+stop_log
+NEXTPID=`pidof $SVC`
+if [ "`getprop init.svc.$SVC`" != stopped ]; then
+  until [ "$PID" != "$NEXTPID" ]; do
+    check_audioserver
+  done
+  killall $PROC
+  check_audioserver
+else
+  start $SVC
+  check_audioserver
+fi
+}
+
+# check
+if [ "$API" -ge 24 ]; then
+  SVC=audioserver
+else
+  SVC=mediaserver
+fi
+PROC=com.fihtdc.am3dfx
+killall $PROC
+check_audioserver
+
+
+
+
+
+
+
+
+
+
 
 
